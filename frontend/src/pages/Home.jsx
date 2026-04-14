8df303b558d82ffad06ed6e4e2a586af80177cea
SanathRai <sanathrai03@gmail.com>
2026-04-14 23:03:09 +0530

    feat: create header structure

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../components/common/Header'
 
 export default function Home() {
   return (
-    <div className='p-4'>
+    <div className='p-12 mx-auto max-w-7xl'>
+      <Header
+        title="Welcome to DaVinci Studio "
+        description="Create stunning visuals from text prompts" 
+      />
       <h1 className="text-3xl font-bold">Home</h1>
     </div>
   )
