a1aaa49980193d42c0705d694cde176f50f73094
SanathRai <sanathrai03@gmail.com>
2026-04-15 22:59:39 +0530

    feat: create image generator structure - FE

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Header from '../components/common/Header'
+import ImageBoard from '../components/common/ImageBoard'
 
 export default function Home() {
   return (
-    <div className='p-12 mx-auto max-w-7xl'>
+    <div className='p-12 mx-auto lg:px-16 max-w-7xl'>
       <Header
         title="Welcome to DaVinci Studio "
         description="Create stunning visuals from text prompts" 
       />
-      <h1 className="text-3xl font-bold">Home</h1>
+      <div className='flex flex-col grid-cols-3 mt-16 md:px-8 lg:px-12 lg:grid md:grid gap-14'>
+        <div className='col-span-2'>
+          <ImageBoard />
+        </div>
+        <div className='col-span-1'>
+
+        </div>
+      </div>
     </div>
   )
 }
