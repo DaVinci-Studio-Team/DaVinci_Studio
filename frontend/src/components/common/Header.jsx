0e1d1c5a7ab08a93837d11f25e0c42ae8450b92e
SanathRai <sanathrai03@gmail.com>
2026-04-19 00:28:07 +0530

    style: improve header style and structure

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { LuSparkles } from "react-icons/lu";
 
-const Header = ({ title, description }) => {
+const Header = () => {
     return (
-        <div className='mb-5 lg:mb-15'>
-            <div className='flex items-center gap-3 mb-2 text-gray-900 text-start font-sans-serif'>
-                <LuSparkles className='inline-block mb-1 text-3xl' />
-                <h1 className='text-[21px] font-semibold lg:text-3xl md:text-3xl'>{title}</h1>
+        <div className='mb-4 text-center lg:mb-16'>
+            <div className="inline-block mb-6 group">
+                <div className="relative flex items-center justify-center w-24 h-24">
+                    <div className="absolute inset-0 transition rounded-full opacity-50 blur-2xl bg-gradient-to-r from-purple-800 via-blue-700 to-pink-700 group-hover:opacity-80"></div>
+                    <LuSparkles className='relative z-10 w-20 h-20 text-purple-500' />
+
+                </div>
             </div>
-            <p className='mt-2 text-center text-gray-400 md:text-start lg:text-start '>{description}</p>
+            <h1 className='mb-6 text-5xl font-bold leading-tight text-transparent md:text-6xl bg-gradient-to-r from-white via-purple-500 to-pink-500 bg-clip-text'>
+                DaVinci Studio
+            </h1>
+            <p className='max-w-3xl mx-auto text-lg leading-relaxed md:text-xl text-muted-foreground'>
+                Empowering creators to turn bold ideas into breathtaking visuals — experience the art of innovation with DaVinci Studio.
+            </p>
+
         </div>
     )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,10 +6,7 @@ import ImageBoard from '../components/common/ImageBoard'
 export default function Home() {
   return (
     <div className='py-12 mx-auto lg:px-16 max-w-7xl'>
-      <Header
-        title="Welcome to DaVinci Studio "
-        description="Create stunning visuals from text prompts" 
-      />
+      <Header/>
       <div className='flex flex-col grid-cols-3 mt-16 md:px-8 lg:px-12 lg:grid md:grid gap-14'>
         <div className='col-span-2'>
           <ImageBoard />
